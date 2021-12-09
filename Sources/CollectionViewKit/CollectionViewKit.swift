f7db3a07035beebd4d310ae7461c49bba666f87f
Viatcheslav Zakharov <vz@moleculus.ru>
2021-12-09 09:48:21 +0100

    Initial Commit

---
+++ Sources/CollectionViewKit/CollectionViewKit.swift
@@ -0,0 +1,6 @@
+public struct CollectionViewKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CollectionViewKitTests/CollectionViewKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CollectionViewKit
+
+final class CollectionViewKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CollectionViewKit().text, "Hello, World!")
+    }
+}
