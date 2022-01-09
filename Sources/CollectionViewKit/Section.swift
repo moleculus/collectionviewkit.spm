b72d2fd046601b94969e507295c869a8872eeecd
Viatcheslav Zakharov <vz@moleculus.ru>
2022-01-09 10:32:13 +0100

    Fixes

--- Sources/CollectionViewKit/Section.swift
+++ Sources/CollectionViewKit/Section.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-open class Section<UI: UIView>: SectionLayoutConfigurator, SectionDataSource {
+open class Section<UI: UIView>: SectionLayoutConfigurator, SectionDataSource, Identifier {
         
     // MARK: - Properties.
     
